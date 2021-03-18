7d179fa1914bdf4d064b93998d4d433fa5c02fd4
angela stephens <angelastephens317@gmail.com>
2021-03-17 20:03:18 -0400

    add function app, import Sundae index, sundae form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  NavLink,
+} from 'react-router-dom'
+import SundaeIndexContainer from './containers/SundaeIndexContainer'
+import SundaeFormContainer from './containers/SundaeFormContainer'
+import SundaeDetailsContainer from './containers/SundaeDetailsContainer'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <Router>
+        <nav className="text-center bg-blue-900 text-yellow-100 p-4">
+          <NavLink
+            className="inline-block px-4 py-2"
+            activeClassName="text-yellow-300"
+            exact
+            to="/"
+          >
+            Sundaes
+          </NavLink>
+          <NavLink
+            className="inline-block px-4 py-2"
+            activeClassName="text-yellow-300"
+            exact
+            to="/sundae/new"
+          >
+            New Sundae
+          </NavLink>
+        </nav>
+        <Switch>
+          <Route exact path="/">
+            <SundaeIndexContainer />
+          </Route>
+            <Route path="/sundae/new">
+            <SundaeFormContainer />
+          </Route>
+          <Route path="/sundaes/:id">
+            <SundaeDetailsContainer />
+          </Route>
+        </Switch>
+      </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
