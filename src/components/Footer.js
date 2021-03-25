08a6f65ffc912496e22a17ae3de088ea8442621f
angela stephens <angelastephens317@gmail.com>
2021-03-24 22:46:46 -0400

    created footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+// import { Footer } from "react-router-dom";
+
+
+function Footer() {
+  return (
+    <div className='empty-container'>
+      <img src='https://thumbs.dreamstime.com/b/vector-colection-set-cute-delicious-looking-cartoon-ice-cream-cones-illustration-152255649.jpg'  width="2000" height="10" ></img>
+    </div>
+  );
+}
+
+export default Footer;
