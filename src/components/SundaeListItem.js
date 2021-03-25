d3d7c5ba827e77f4078e9246143475fdc345f7e9
angela stephens <angelastephens317@gmail.com>
2021-03-24 22:47:44 -0400

    updae sundaelistitem

--- src/components/SundaeListItem.js
+++ src/components/SundaeListItem.js
@@ -3,14 +3,19 @@ import {Link} from 'react-router-dom'
 
 const SundaeListItem = ({ sundae }) => {
   return (
-    <li className="" key={sundae.id}>
-      <Link to={`/sundaes/${sundae.id}`} >name {sundae.name} </Link>
-      Scoop count {sundae.scoops}
+    <li
+      className='border-dotted border-4 border-yellow-200 max-w-6xl w-2/4 my-8 shadow-md px-4 py-6'
+      key={sundae.id}
+    >
+      <Link to={`/sundaes/${sundae.id}`}>
+        <strong> Name: </strong> {sundae.name}{" "}
+      </Link>
+      {/* Scoop count {sundae.scoops}
       Ice Cream Flavors {sundae.ice_cream_flavors}<br/>
       Toppings {sundae.toppings}<br/>
-      Like Count {sundae.like_count}
+      Like Count {sundae.like_count} */}
     </li>
-  )
+  );
 }
 
 export default SundaeListItem
