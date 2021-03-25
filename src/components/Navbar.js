e11194167ea06a78f2e6e5fe585c89fd8ba4c111
angela stephens <angelastephens317@gmail.com>
2021-03-24 22:46:59 -0400

    create navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav className='text-center bg-pink-900 text-yellow-100 p-4'>
+      <NavLink
+        className='inline-block px-4 py-2'
+        activeClassName='text-yellow-300'
+        exact
+        to='/'
+      >
+        Sundaes
+      </NavLink>
+      <NavLink
+        className='inline-block px-4 py-2'
+        activeClassName='text-yellow-300'
+        exact
+        to='/sundae/new'
+      >
+        New Sundae
+      </NavLink>
+    </nav>
+  );
+}
+
+export default Navbar;
