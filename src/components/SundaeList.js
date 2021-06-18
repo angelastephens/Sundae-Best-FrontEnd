63eb4d7925d52ce3ab778fa1d8f7da1c0847a597
angela stephens <angelastephens317@gmail.com>
2021-06-17 22:06:16 -0400

    add sort to sundae list

--- src/components/SundaeList.js
+++ src/components/SundaeList.js
@@ -1,19 +1,30 @@
-import React from 'react'
-import SundaeListItem from './SundaeListItem'
+import React from "react";
+import SundaeListItem from "./SundaeListItem";
 
-const SundaeList = ({sundaes}) => {
+const SundaeList = ({ sundaes, foo }) => {
+  const sortedSundaes = sundaes.sort(function (a, b) {
+    return b.like_count - a.like_count;
+  });
+
+  // const mostLikeSundaes = sundaes.filter(function (sundae) {
+  //   return sundae.like_count > 15;
+  // });
+
+  // mostLikeSundaes;
+
   return (
     <>
-      <h1 className= "text-3xl" > Sundae List</h1>
-      <ul>
-        {sundaes.map((sundae) => (
+      <h1 className='text-3xl'> Sundae List</h1>
+      <h1> {foo} </h1>
+
+      
+      <ul>  
+        {sortedSundaes.map((sundae) => (               
           <SundaeListItem key={sundae.id} sundae={sundae} />
         ))}
       </ul>
     </>
-  )
-}
+  );
+};
 
-
-
-export default SundaeList
+export default SundaeList;
