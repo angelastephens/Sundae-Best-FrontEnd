b7bbbde5a84fdfb493620b2fd577b0a584846e07
angela stephens <angelastephens317@gmail.com>
2021-06-17 22:05:17 -0400

    commented out counter code

---
+++ src/containers/counter.js
@@ -0,0 +1,30 @@
+// import React from 'react';
+
+// class Counter extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       count: 0
+//     };
+//   }
+
+//   increment() {
+//     this.setState({
+//       count: this.state.count + 1
+//     });
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <button className='add' onClick={(e) => this.increment(e)}>Increment!</button>
+      
+//         <h1>Count: {this.state.count}</h1>
+//       </div>
+//     );
+//   }
+// }
+
+
+// export default Counter 
+  
