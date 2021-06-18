7c4b7d75b5df363f0b64aa77c41a937eefed39de
angela stephens <angelastephens317@gmail.com>
2021-06-17 22:04:47 -0400

    removed state from sundae details container

--- src/containers/SundaeDetailsContainer.js
+++ src/containers/SundaeDetailsContainer.js
@@ -5,47 +5,12 @@ import SundaeDetailswithBorder from '../components/SundaeDetailswithBorder';
 
 
 class SundaeDetailsContainer extends React.Component {
-  // state = {
-  //   sundae: {},
-  //   loading: false
-  // };
+  
   componentDidMount() {
      const sundaeId = this.props.match.params.id;
     this.props.dispatchFetchSundae(sundaeId);
   }
-  //   const sundaeId = this.props.match.params.sundaeId;
-  //   fetch(`http://localhost:3001/sundaes/${sundaeId}`)
-  //     .then((res) => res.json())
-  //     .then(({ sundae }) => {
-  //       this.setState({
-  //         sundae: sundae,
-  //         loading: false
-  //       });
-  //     });
-  // }
-  // componentDidMount() {
-  //   if(this.props.loadingState === "notStarted") {
-  //     this.props.dispatchFetchSundaes()
-  //   }
-  // }
-  //     const id = this.props.match.params.id
-
-  //     fetch(`http://localhost:3001/sundaes/${id}`, {
-  //       method: 'get',
-  //       headers: {
-  //         'Accept': 'application/json',
-  //         'Content-Type': 'application/json'
-  //       }
-  //     })
-  //     .then(res => res.json())
-  //     .then(sundaeJson => {
-  //       console.log('sundaes', sundaeJson)
-  //       this.setState({
-  //         sundae: sundaeJson,
-  //         loading: false
-  //       })
-  //     })
-  // }
+ 
 
   render() {
     if (this.props.loading) {
@@ -56,16 +21,6 @@ class SundaeDetailsContainer extends React.Component {
       <div>
         <SundaeDetailswithBorder sundae={this.props.sundae} />
       </div>
-
-      // <div className='max-w-6xl w-3/4 mx-auto '>
-      //   <h1 className='text-3xl'> Sundae Ingredients</h1>
-      //   <ul className=' border-dotted border-4 border-light-blue-500 mt-4 shadow-lg px-4 py-6 mb-4 w-1/2'>
-      //     {this.props.sundae.name} <br />
-      //     Number of Scoops: {this.props.sundae.scoops} <br />
-      //     Ice Cream Flavors: {this.props.sundae.ice_cream_flavors} <br />
-      //     Toppings: {this.props.sundae.toppings}
-      //   </ul>
-      // </div>
     );
   }
 }
@@ -79,10 +34,8 @@ const mapStateToProps = (state, { match }) => {
     loadingState
   }
 
-  //   loadingState
-  // };
 };
-const mapDispatchToProps = (dispatch) => {
+const mapDispatchToProps = (dispatch) => { //function that takes in sundaeId, dispatch action return   thunk lets me dispatch a function, and will call it for me , without thunk i can only dispatch objects 
   return {
     dispatchFetchSundae: (sundaeId) => dispatch(fetchSundae(sundaeId))
   };
