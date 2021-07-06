6e7c4f2d403d695e8c1ba69cc293a3a6fa41a0ae
angela stephens <angelastephens317@gmail.com>
2021-07-05 21:59:00 -0400

    add nenw image to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,9 +5,12 @@ import React from "react";
 function Footer() {
   return (
     <div className='empty-container'>
-      <img src='https://thumbs.dreamstime.com/b/vector-colection-set-cute-delicious-looking-cartoon-ice-cream-cones-illustration-152255649.jpg'  width="2000" height="10" ></img>
+      <img src='https://t4.ftcdn.net/jpg/03/36/60/49/240_F_336604943_P6Yu1BhHyA5PyKFXaBGKv6UlhMYOPgWe.jpg'  width="2000" height="10" ></img>
     </div>
   );
 }
 
 export default Footer;
+
+
+//<img src='https://thumbs.dreamstime.com/b/vector-colection-set-cute-delicious-looking-cartoon-ice-cream-cones-illustration-152255649.jpg'  width="2000" height="10" ></img>
