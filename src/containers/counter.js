31a20d9ea5c2b9f59cea3be927d5e3715a163d86
angela stephens <angelastephens317@gmail.com>
2021-07-19 22:42:42 -0400

    add in counter code

--- src/containers/counter.js
+++ src/containers/counter.js
@@ -1,30 +1,30 @@
-// import React from 'react';
+import React from 'react';
 
-// class Counter extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       count: 0
-//     };
-//   }
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    };
+  }
 
-//   increment() {
-//     this.setState({
-//       count: this.state.count + 1
-//     });
-//   }
+  increment() {
+    this.setState({
+      count: this.state.count + 1
+    });
+  }
 
-//   render() {
-//     return (
-//       <div>
-//         <button className='add' onClick={(e) => this.increment(e)}>Increment!</button>
+  render() {
+    return (
+      <div>
+        <button className='add' onClick={(e) => this.increment(e)}>Increment!</button>
       
-//         <h1>Count: {this.state.count}</h1>
-//       </div>
-//     );
-//   }
-// }
+        <h1>Count: {this.state.count}</h1>
+      </div>
+    );
+  }
+}
 
 
-// export default Counter 
+export default Counter 
   
