3aecb74cd02efac0fd83a3faadf71f699959c0e7
angela stephens <angelastephens317@gmail.com>
2021-08-16 19:42:41 -0400

    add additional button

--- src/components/SundaeListItem.js
+++ src/components/SundaeListItem.js
@@ -39,6 +39,14 @@ class SundaeListItem extends React.Component {
         >
           <strong>Boo for this Sundae: </strong> {this.props.sundae.boo_vote}
         </button>
+
+        <button
+          className='w-full p-4 bg-pink-300 mt-4 hover:bg-purple-400 transition-all duration-200'
+          onClick={() => this.booCounter()}
+        >
+          <strong>: </strong> 
+        </button>
+    
       </li>
     );
   }
