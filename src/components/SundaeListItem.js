83e2f17d5bef5b038acbb8d633d981d568b84756
angela stephens <angelastephens317@gmail.com>
2021-09-06 22:03:14 -0400

    changed spelling

--- src/components/SundaeListItem.js
+++ src/components/SundaeListItem.js
@@ -31,7 +31,7 @@ class SundaeListItem extends React.Component {
         >
           {" "}
           <strong>Cheer for this Sundae:</strong> {this.props.sundae.like_count}{" "}
-          total cheers{" "}
+          total cheer{" "}
         </button>
         <button
           className='w-full p-4 bg-pink-300 mt-4 hover:bg-purple-400 transition-all duration-200'
