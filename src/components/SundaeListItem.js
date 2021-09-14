45d0f1ae132b6b84552a21c11fb2f51e30f44663
angela stephens <angelastephens317@gmail.com>
2021-09-13 21:22:40 -0400

    add S

--- src/components/SundaeListItem.js
+++ src/components/SundaeListItem.js
@@ -31,7 +31,7 @@ class SundaeListItem extends React.Component {
         >
           {" "}
           <strong>Cheer for this Sundae:</strong> {this.props.sundae.like_count}{" "}
-          total cheer{" "}
+          total cheers{" "}
         </button>
         <button
           className='w-full p-4 bg-pink-300 mt-4 hover:bg-purple-400 transition-all duration-200'
