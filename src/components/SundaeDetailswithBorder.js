bda03688840c913f02e287579c2e0a8aaa2a3a5a
angela stephens <angelastephens317@gmail.com>
2021-11-08 21:51:19 -0500

    corrected the title

--- src/components/SundaeDetailswithBorder.js
+++ src/components/SundaeDetailswithBorder.js
@@ -3,7 +3,7 @@ import React from "react";
 const SundaeDetailswithBorder = ({sundae}) => {
   return (
     <div className='max-w-6xl w-3/4 mx-auto '>
-      <h1 className='text-3xl'> Enter your ingredients</h1>
+      <h1 className='text-3xl'> Sundae Ingredients</h1>
       <ul className=' border-dotted border-4 border-light-blue-500 mt-4 shadow-lg px-4 py-6 mb-4 w-1/2'>
         {sundae.name} <br />
         Number of Scoops: {sundae.scoops} <br />
