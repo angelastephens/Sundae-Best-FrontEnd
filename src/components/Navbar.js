79523ebcfc3cd8f1a0c5f3950690ea942ba152af
angela stephens <angelastephens317@gmail.com>
2021-11-29 21:45:02 -0500

    removed word set

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,7 +10,7 @@ function Navbar() {
         exact
         to='/'
       >
-        All The Yummy in the  Sundaes
+        All The Yummy in   Sundaes
       </NavLink>
       <NavLink
         className='inline-block px-4 py-2'
