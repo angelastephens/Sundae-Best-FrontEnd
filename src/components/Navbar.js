8dfb60702a857386d7e79604e13fdaa6a53d4cdd
angela stephens <angelastephens317@gmail.com>
2021-12-06 21:59:01 -0500

    add descriptive word

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,7 +10,7 @@ function Navbar() {
         exact
         to='/'
       >
-        All The Yummy Sundaes
+        All The Best Sundaes
       </NavLink>
       <NavLink
         className='inline-block px-4 py-2'
