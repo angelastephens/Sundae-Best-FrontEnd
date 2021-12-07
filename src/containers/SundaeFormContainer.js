7f7debe39f0a3e2d7f2faf42b8e0d1a032cad228
angela stephens <angelastephens317@gmail.com>
2021-12-06 22:04:50 -0500

    word change for submit

--- src/containers/SundaeFormContainer.js
+++ src/containers/SundaeFormContainer.js
@@ -80,7 +80,7 @@ class SundaeFormContainer extends Component {
           className='w-full p-4 bg-pink-300 mt-4 hover:bg-purple-400 transition-all duration-200'
           type='submit'
         >
-          Add Your Scoop-tastic Sundae
+          Load Your Scoop-tastic Sundae
         </button>
         <li className='w-full p-4 bg-pink-300 mt-4 hover:bg-purple-400 transition-all duration-200'>
           <strong>Most Used Toppings:{this.state.toppings} </strong>
