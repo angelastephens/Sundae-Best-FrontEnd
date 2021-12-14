dde2e9a3af2f200427140a3e3da4320f03f2f5c8
angela stephens <angelastephens317@gmail.com>
2021-12-13 21:50:11 -0500

    remove .state

--- src/containers/SundaeFormContainer.js
+++ src/containers/SundaeFormContainer.js
@@ -83,7 +83,7 @@ class SundaeFormContainer extends Component {
           Load Your Scoop-tastic Sundae
         </button>
         <li className='w-full p-4 bg-pink-300 mt-4 hover:bg-purple-400 transition-all duration-200'>
-          <strong>:{this.state.toppings} </strong>
+          <strong>: </strong>
         </li>
       </form>
     );
