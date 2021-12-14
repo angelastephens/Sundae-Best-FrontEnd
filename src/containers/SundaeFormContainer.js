7553c23fdf2ede663716b6d325120a9e4b9190c0
angela stephens <angelastephens317@gmail.com>
2021-12-13 21:49:49 -0500

    remove words

--- src/containers/SundaeFormContainer.js
+++ src/containers/SundaeFormContainer.js
@@ -83,7 +83,7 @@ class SundaeFormContainer extends Component {
           Load Your Scoop-tastic Sundae
         </button>
         <li className='w-full p-4 bg-pink-300 mt-4 hover:bg-purple-400 transition-all duration-200'>
-          <strong>Most Used Toppings:{this.state.toppings} </strong>
+          <strong>:{this.state.toppings} </strong>
         </li>
       </form>
     );
